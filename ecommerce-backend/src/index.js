68b7ae1e057ce4d040d899c72a8830a64f9e4283
Xin chào Hiếu !!! <Nguyenhieu6732@gmail.com>
2024-12-23 22:13:59 +0700

    first commit

---
+++ ecommerce-backend/src/index.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const routes = require("./routes")
+const cors = require('cors')
+const { default: mongoose } = require("mongoose")
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3001
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'token'],
+    credentials: true,
+}))
+app.use(express.json({ limit: "50mb" }))
+app.use(express.urlencoded({ limit: "50mb" }))
+
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+routes(app)
+
+
+
+
+mongoose.connect(`${process.env.MONGO_DB}`)
+    .then(() => {
+        console.log("Connect to database!")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+app.listen(port, () => {
+    console.log("Server is running in port", + port)
+})
